80ecb0a3bf31dbaab37c28ab2077567c4068658b
ataknz <atakanzzengin@gmail.com>
2021-05-31 20:54:37 +0300

    Minor refactor on class structure

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,25 @@
-import { Client } from "@notionhq/client/build/src";
-import { SearchFilter } from "@notionhq/client/build/src/api-types";
-import { config } from "dotenv";
 import { NotionClient } from "./notion";
 import { Prompt } from "./prompt";
-
-config({ path: `${process.cwd()}/env/.env` });
 
 const main = async () => {
   const prompt = new Prompt();
   const answers = await prompt.getAnswers();
 
-  const client = new Client({ auth: answers["notionToken"] });
-
-  const notion = new NotionClient(client);
+  const notion = new NotionClient(answers["notionToken"]);
 
   if (answers["getUsers"]) {
-    console.log("\nGetting users from workspace...");
-    const users = await notion.getUsers();
-    console.log("Users:");
-    console.log(users);
+    await notion.getUsers();
   }
 
   if (answers["getDB"]) {
-    console.log("\nRetrieving database...");
-    const database = await notion.retrieveDatabase(answers["dbID"]);
-    console.log("Database:");
-    console.log(database);
+    await notion.retrieveDatabase(answers["dbID"]);
   }
 
   if (answers["searchWorkspace"]) {
-    console.log("\nSearching workspace...");
     const pageOrDB = answers["pageOrDB"];
     const query = answers["titleQuery"];
 
-    const filterBy: SearchFilter = {
-      property: "object",
-      value: pageOrDB,
-    };
-
-    const searchResponse = await notion.searchWorkspace(query, filterBy);
-
-    console.log("\nSearch Response:");
-    console.log(searchResponse);
+    await notion.searchWorkspace(query, pageOrDB);
   }
 
   console.log("\nTake care stranger!");
--- src/notion.ts
+++ src/notion.ts
@@ -1,41 +1,45 @@
-import { APIResponseError, Client } from "@notionhq/client/build/src";
-import { SearchFilter } from "@notionhq/client/build/src/api-types";
+import { Client } from "@notionhq/client/build/src";
 
 export class NotionClient {
   private _notionClient: Client;
 
-  constructor(notion: Client) {
-    this._notionClient = notion;
+  constructor(auth: string) {
+    this._notionClient = new Client({ auth: auth });
   }
 
   getUsers = async () => {
     try {
-      return await this._notionClient.users.list();
+      const users = await this._notionClient.users.list();
+      console.log("\nUsers:");
+      console.log(users);
     } catch (err) {}
   };
 
-  retrieveDatabase = async (databaseID?: string) => {
+  retrieveDatabase = async (databaseID: string) => {
     try {
-      if (databaseID == undefined) {
-        throw new Error("DATABASE_ID environment variable is undefined");
-      }
-
-      return await this._notionClient.databases.retrieve({
+      const database = await this._notionClient.databases.retrieve({
         database_id: databaseID,
       });
+      console.log("\nDatabase Result:");
+      console.log(database);
     } catch (err) {}
   };
 
-  searchWorkspace = async (query: string, filterBy?: SearchFilter) => {
+  searchWorkspace = async (query: string, pageOrDB: "page" | "database") => {
     try {
-      return await this._notionClient.search({
+      const search = await this._notionClient.search({
         query: query,
-        filter: filterBy,
+        filter: {
+          property: "object",
+          value: pageOrDB,
+        },
         sort: {
           direction: "descending",
           timestamp: "last_edited_time",
         },
       });
+      console.log("\nSearch Result:");
+      console.log(search);
     } catch (err) {}
   };
 }
